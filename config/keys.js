df78cb2c3f36f44a194a4d6c4acdeb912259f7b3
Dan Perkins <67369794+Perkins-Dan@users.noreply.github.com>
2020-10-10 11:33:31 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://Dan:dLv7BAZ0XNQkRgBw@cluster0.5murj.mongodb.net/LoginTest?retryWrites=true&w=majority"
+    "mongodb+srv://Dan:#########@cluster0.5murj.mongodb.net/LoginTest?retryWrites=true&w=majority"
 };
